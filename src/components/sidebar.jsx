8f5d1bf70c5ec10ee6405495f1e156724f4336d9
SREELAKSHMI S <sreelakshmis112001@gmail.com>
2025-04-01 19:39:47 +0530

    sidebar added

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -2,7 +2,6 @@ function Sidebar() {
     return (
         <div className="d-flex">
             <div className="bg-dark text-white vh-100 p-3" style={{ width: "250px" }}>
-                <h4>Sidebar</h4>
                 <ul className="nav flex-column">
                     <li className="nav-item"><a href="#" className="nav-link text-white">Home</a></li>
                     <li className="nav-item"><a href="#" className="nav-link text-white">About</a></li>
