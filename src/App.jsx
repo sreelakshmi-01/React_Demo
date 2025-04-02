88a5705cf8369176b319b5ba79ec82ba917dd7a9
SREELAKSHMI S <sreelakshmis112001@gmail.com>
2025-04-02 11:09:00 +0530

    addSubtract file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import ColorSchemesExample from "./components/navbar"
 import Sidebar from "./components/sidebar"
+import AddSub from "./components/addsubtract"
 
 function App() {
   return (
     <div>
       <ColorSchemesExample></ColorSchemesExample> 
       <Sidebar></Sidebar>
+      <AddSub></AddSub>
     </div>
+    
   )
 }
 
---
+++ src/components/addsubtract.jsx
@@ -0,0 +1,10 @@
+function AddSub() {
+    let name = "Gopika"
+    return (
+        <div>
+            <h1>Name: {name}</h1>
+        </div>
+    )
+}
+
+export default AddSub;
