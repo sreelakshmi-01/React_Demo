56c433bf679c669c5cf2577761218d963470e533
SREELAKSHMI S <sreelakshmis112001@gmail.com>
2025-04-07 10:21:24 +0530

    app.jsx edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import ColorSchemesExample from "./components/navbar"
 import Sidebar from "./components/sidebar"
 import Electronics from "./components/electronicsdata"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div>
-      <ColorSchemesExample></ColorSchemesExample>
-      <div className="d-flex">
-        <Sidebar />
-        <div className="ms-3">
-          <Electronics></Electronics>
-        </div>
-      </div>
-    </div>
-
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/add" element={<AddData />}></Route>
+        <Route path="/more/:id" element={<More />}></Route>
+        <Route path="/fetch" element={<FetchUsers />}></Route>
+      </Routes>
+    </>
   )
 }
 
