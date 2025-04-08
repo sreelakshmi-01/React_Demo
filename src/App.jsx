322ab135f8123cfc41a3f8bc83a9753ae3700b5e
SREELAKSHMI S <sreelakshmis112001@gmail.com>
2025-04-08 12:10:06 +0530

    add product

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import ColorSchemesExample from "./components/navbar"
 import Electronics from "./components/electronicsdata"
 import Sidebar from "./components/sidebar"
-import { Routes,Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import ViewMore from "./components/viewmore"
 import Add from "./components/Add"
 
 function App() {
   return (
     <>
-    <ColorSchemesExample></ColorSchemesExample>
-    <div style={{display:'flex'}}>
-    <Sidebar></Sidebar>
-    <div style={{flex:'1',padding:'20px'}}>
-    <Routes>
-      <Route path="/" element={<Electronics/>}></Route>
-      <Route path="more/:electronic_id" element={<ViewMore/>}></Route>
-      <Route path="/add" element={<Add/>}></Route>
-    </Routes>
-    </div>
-    </div>
+      <ColorSchemesExample></ColorSchemesExample>
+      <div style={{ display: 'flex' }}>
+        <Sidebar></Sidebar>
+        <div style={{ flex: '1', padding: '20px' }}>
+          <Routes>
+            <Route path="/" element={<Electronics />}></Route>
+            <Route path="more/:electronic_id" element={<ViewMore />}></Route>
+            <Route path="/add" element={<Add />}></Route>
+          </Routes>
+        </div>
+      </div>
     </>
   )
 }
